a280eb87cd6ab71869c4b9cf0a14f83ec71f2a13
512-kb <kunal.bhardwaj@codequotient.com>
2021-01-27 10:44:46 +0530

    routes fix

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const routes = require("./routes");
+const path = require("path");
 require("../database");
 
 app.use(
@@ -15,6 +16,10 @@ app.use(express.json());
 app.use(routes);
 
 app.use(express.static(`${__dirname}/../public/build`));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "build", "index.html"));
+});
 
 module.exports = function constructor(settings) {
   const { port } = settings;
